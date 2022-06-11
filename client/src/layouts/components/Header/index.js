bcb026a706e4c6dc25cfbc66f47450b5b20e8c4e
ndbao <nguyenducbao166@gmail.com>
2022-06-11 21:30:37 +0700

    menu icon

--- client/src/layouts/components/Header/index.js
+++ client/src/layouts/components/Header/index.js
@@ -2,21 +2,38 @@ import classNames from 'classnames/bind';
 import { Link } from 'react-router-dom';
 
 import style from './header.module.scss';
-import { LogoHeader } from '~/Components/icons';
+import { LogoHeader, HomeIcon, TiviIcon, IconMarket, IconGroup } from '~/Components/icons';
 import config from '~/config';
 import Search from '../Search';
+import Button from '~/Components/Button';
 
 const cx = classNames.bind(style);
 
 function Header() {
   return (
     <header className={`header ${cx('header')}`}>
-      <div className={cx('wrapper')}>
+      <div className={cx('wrapper-menu-left')}>
         <Link to={config.router.home} className={cx('logo-header')}>
           <LogoHeader width="4.8rem" height="4.8rem" />
         </Link>
         <Search />
       </div>
+      <div className={cx('wrapper-icon')}>
+        <Button className={cx('icon')} large>
+          <HomeIcon width="3.1rem" height="3.1rem" />
+        </Button>
+        <Button className={cx('icon')} large>
+          <TiviIcon width="3.1rem" height="3.1rem" />
+        </Button>
+        <Button className={cx('icon')} large>
+          <IconMarket width="3.1rem" height="3.1rem" />
+        </Button>
+        <Button className={cx('icon')} large>
+          <IconGroup width="3.1rem" height="3.1rem" />
+        </Button>
+      </div>
+
+      <div style={{ width: '350px' }}>User</div>
     </header>
   );
 }
