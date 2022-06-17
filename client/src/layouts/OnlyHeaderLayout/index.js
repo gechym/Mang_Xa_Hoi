365c45fc9db69289fb94458f6444c8618fc35029
ndbao <nguyenducbao166@gmail.com>
2022-06-17 22:39:18 +0700

    style Page Profile

--- client/src/layouts/OnlyHeaderLayout/index.js
+++ client/src/layouts/OnlyHeaderLayout/index.js
@@ -1,17 +1,13 @@
 import classNames from 'classnames/bind';
 import style from './onlyHeaderLayout.module.scss';
 import Header from '~/layouts/components/Header';
-import SiderBar from '~/layouts/components/siderBar';
 const cx = classNames.bind(style);
 
 function OnlyHeaderLayput({ children }) {
   return (
     <div className={cx('container')}>
       <Header />
-      <div className={cx('body')}>
-        <SiderBar width={400} />
-        {children}
-      </div>
+      <div className={cx('body')}>{children}</div>
     </div>
   );
 }
