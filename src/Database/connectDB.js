6c260a4a0cf0d39a51b855f5945aae5bda80810d
ndbao <nguyenducbao166@gmail.com>
2022-06-22 20:54:38 +0700

    association Post - Comment - reply - like

--- src/Database/connectDB.js
+++ src/Database/connectDB.js
@@ -1,7 +1,11 @@
+import { sequelize } from './serviceDatabase';
 import UserInfo from '../module/UserInfo';
 import User from '../module/User';
-import { sequelize } from './serviceDatabase';
 import UserRelationship from '../module/UserRelationship';
+import Post from '../module/Post';
+import Comment from '../module/Comment';
+import RepLyComment from '../module/RepComments';
+import Like from '../module/Like';
 
 const connectDatabase = async () => {
   try {
@@ -14,7 +18,31 @@ const connectDatabase = async () => {
     UserInfo.hasMany(UserRelationship, { as: 'userReciver', foreignKey: 'user_reciver' });
     UserRelationship.belongsTo(UserInfo, { as: 'userReciver', foreignKey: 'user_reciver' });
 
-    // sequelize
+    UserInfo.hasMany(Post, { as: 'posts', foreignKey: 'user_id' });
+    Post.belongsTo(UserInfo, { as: 'posts', foreignKey: 'user_id' });
+
+    Post.hasMany(Comment, { as: 'commentsPost', foreignKey: 'post_id' });
+    Comment.belongsTo(Post, { as: 'commentsPost', foreignKey: 'post_id' });
+
+    UserInfo.hasMany(Comment, { as: 'comments', foreignKey: 'user_id' });
+    Comment.belongsTo(UserInfo, { as: 'comments', foreignKey: 'user_id' });
+
+    Comment.hasMany(RepLyComment, { as: 'replyComments', foreignKey: 'comment_id' });
+    RepLyComment.belongsTo(Comment, { as: 'replyComments', foreignKey: 'comment_id' });
+
+    UserInfo.hasMany(RepLyComment, { as: 'userReplyComment', foreignKey: 'user_id' });
+    RepLyComment.belongsTo(UserInfo, { as: 'userReplyComment', foreignKey: 'user_id' });
+
+    Comment.hasMany(Like, { as: 'likedComments', foreignKey: 'comment_id' });
+    Like.belongsTo(Comment, { as: 'likedComments', foreignKey: 'comment_id' });
+
+    UserInfo.hasMany(Like, { as: 'userLike', foreignKey: 'user_id' });
+    Like.belongsTo(UserInfo, { as: 'userLike', foreignKey: 'user_id' });
+
+    Post.hasMany(Like, { as: 'postLike', foreignKey: 'post_id' });
+    Like.belongsTo(Post, { as: 'postLike', foreignKey: 'post_id' });
+
+    // await sequelize
     //   .sync({ force: true })
     //   .then((result) => {
     //     console.log('\n\n\n👉 Đồng bộ server thành công \n\n\n');
