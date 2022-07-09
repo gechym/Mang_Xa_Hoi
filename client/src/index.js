680df3bc1bca0f075f60ff107e21469d13645e44
ndbao <nguyenducbao166@gmail.com>
2022-07-09 15:14:44 +0700

    set up react query

--- client/src/index.js
+++ client/src/index.js
@@ -3,16 +3,30 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { Provider } from 'react-redux';
 import { ThemeProvider } from '@material-tailwind/react';
+import { QueryClientProvider, QueryClient } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 
 import store from './redux/store';
 
 import './index.css';
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <ThemeProvider>
     <Provider store={store}>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+        <ReactQueryDevtools />
+      </QueryClientProvider>
     </Provider>
   </ThemeProvider>,
 );
