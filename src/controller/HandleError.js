3c283af29f2a7781d3448816b4638fbe371cbb6f
ndbao <nguyenducbao166@gmail.com>
2022-07-10 22:09:01 +0700

    custom error message

--- src/controller/HandleError.js
+++ src/controller/HandleError.js
@@ -9,8 +9,21 @@ const handleError = () => (err, req, res, next) => {
   );
 
   const statusCode = err.statusCode || 500;
+
+  console.log(err.message);
+
+  let customError = false;
+
+  if (err.message.includes('SequelizeUniqueConstraintError:')) {
+    customError = 'Đã có người đăng ký bằng email này, vui lòng thử email khác';
+  }
+
+  if (err.message.includes('SequelizeValidationError: Validation error:')) {
+    customError = err.message.split('Validation error: ')[1];
+  }
+
   return res.status(statusCode).json({
-    message: err.message,
+    message: customError ? customError : err.message,
   });
 };
 
